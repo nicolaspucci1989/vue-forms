d69f7115b64e516a4bca7281269f14074b76cce7
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-05-24 22:49:39 -0300

    feat: add base input component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="form-group">
+    <label>{{ label }}</label>
+    <input
+        type="text"
+        class="form-control"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    >
+  </div>
+</template>
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    default: 'text',
+    validator(value) {
+      return ['text', 'email', 'password'].includes(value)
+    }
+  },
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+defineEmits(['update:modelValue'])
+</script>
+<style lang="sass" scoped>
+a
+  color: #42b983
+</style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {computed, ref} from "vue";
 import axios from 'axios'
+import BaseInput from "./BaseInput.vue";
 
 const form = ref({
   firstName: '',
@@ -15,13 +16,14 @@ const formIsValid = computed(() =>
         form.value.email.length > 0
     ))
 
-function doSubmit(payload) {
+function doSubmit() {
   if (!formIsValid.value) return
   axios
       .post('http://localhost:3001/content', {params: {...form.value}})
       .then(res => {
         console.log('Form has been posted', res)
-      }).catch(err => {
+      })
+      .catch(err => {
         console.log('An error has occured', err)
       })
 }
@@ -32,23 +34,32 @@ function doSubmit(payload) {
     <div class="row">
       <div class="col-12">
         <form @submit.prevent="doSubmit">
-          <div class="form-group">
-            <label>First Name</label>
-            <input type="text" class="form-control" v-model="form.firstName">
-          </div>
+          <BaseInput
+              label="First Name"
+              v-model="form.firstName"
+          />
+
+          <BaseInput
+              label="Last Name"
+              v-model="form.lastName"
+          />
+
+          <BaseInput
+              label="Email"
+              v-model="form.email"
+              type="email"
+          />
 
           <div class="form-group">
-            <label>Last Name</label>
-            <input type="text" class="form-control" v-model="form.lastName">
+            <button
+                :disabled="!formIsValid"
+                @click.prevent="doSubmit"
+                type="submit" class="btn btn-primary"
+            >
+              Submit
+            </button>
           </div>
 
-          <div class="form-group">
-            <label>Email</label>
-            <input type="Email" class="form-control" v-model="form.email">
-          </div>
-          <div class="form-group">
-            <button :disabled="!formIsValid" @click.prevent="doSubmit" type="submit" class="btn btn-primary">Submit</button>
-          </div>
         </form>
         <pre>
           {{ form }}
