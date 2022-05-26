87808036482cebdeed6076190a3039bc5cc70782
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-05-25 23:15:01 -0300

    add pinia

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Maska from "maska";
+import {createPinia} from "pinia";
 
 createApp(App)
     .use(Maska)
+    .use(createPinia())
     .mount('#app')
