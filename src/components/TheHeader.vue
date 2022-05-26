a8c09fe2a0bc772909c7ed64a1b1b97d7e895cb3
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-05-26 01:26:16 -0300

    minor changes

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,13 +1,14 @@
 <script setup>
 import {useStore} from "../store";
-
 const store = useStore()
+const name = store.user.firstName
+
 </script>
 <template>
   <div class="row">
     <div class="col-12 text-right">
       <p v-if="store.$state.user">
-        Welcome back, {{ store.$state.user.firstName }}!
+        Welcome back, {{ name }}!
       </p>
     </div>
   </div>
